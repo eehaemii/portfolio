35f01b4684279a269cc1f8d47d931fc44ba43e85
eehaemii <eehaemii@example.com>
2021-08-06 11:51:24 +0900

    portfolio layout 변경

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,37 +1,4 @@
 $(function(){
-  
-       var swiper = new Swiper(".mySwiper", {
-            direction: "vertical",
-            slidesPerView: 1,
-            spaceBetween: 30,
-            mousewheel: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
-
-        var swiper = new Swiper(".mySwiper2", {
-            cssMode: true,
-            mousewheel: true,
-            keyboard: true,
-            slidesPerView: 1,
-            spaceBetween: 30,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-
-        $(document).ready(function(){
-            $('.typed').typed({
-                strings:["HAEMI \<br\>PORTFOLIO"],
-                typeSpeed: 70,
-                backDelay:200,
-                loop:false
-            })
-        });
 
 });
 
